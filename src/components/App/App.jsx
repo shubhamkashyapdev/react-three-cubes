da5cb020753a7a2e7fbdacf14ac992de68839586
Shubham Kashyap <hex.media.1800@gmail.com>
2022-01-23 22:43:58 +0530

    floor is added with some point lights

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,8 +8,31 @@ function App() {
     <>
       <Canvas colorManagement camera={{ position: [-5, 2, 10], fov: 60 }}>
         <ambientLight intensity={0.3} />
+        <directionalLight
+          intensity={1.5}
+          position={[0, 10, 0]}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+        <pointLight position={[-10, 0 - 20]} intensity={0.5} />
+        <pointLight position={[0, -10, 0]} intensity={1.5} />
 
-        <SpinningBox position={[0, 1, 0]} color='lightblue' args={[1, 1, 1]} />
+        {/* floor is needed to cast the shadow */}
+        <group>
+          <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -3, 0]}>
+            <planeBufferGeometry attach='geometry' args={[100, 100]} />
+            <meshStandardMaterial attach='material' color='yellow' />
+          </mesh>
+        </group>
+
+        <SpinningBox position={[0, 1, 0]} color='lightblue' args={[3, 2, 1]} />
+        <SpinningBox position={[-3, 1, -5]} color='pink' />
+        <SpinningBox position={[5, 1, -1]} color='pink' />
       </Canvas>
     </>
   )
